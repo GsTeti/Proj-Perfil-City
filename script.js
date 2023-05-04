0b86d4f0ab78110d03011d5fae1e7713667caa21
Giovana Stefani <101645617+GsTeti@users.noreply.github.com>
2023-05-04 12:18:51 -0300

    trabaio
    Falta pasta img vazia

---
+++ script.js
@@ -0,0 +1,7 @@
+alert("Ola Seja Bem-vindo")
+
+let p1=prompt("Digite Seu NOME");
+
+let msg= document.getElementByld(msg)
+
+msg.innerHTML=<p>Ola $(p1) o seu poder e a/o $(p2) você vai enfrentar o/a $(p3) np/em $(p4)</p>
